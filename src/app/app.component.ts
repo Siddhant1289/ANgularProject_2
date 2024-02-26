3f58a417eb02e80b5212c4a8c2813516fd7929e4
Siddhant Verma <karanspd1289@gmail.com>
2024-02-26 13:04:18 +0530

    Created Form Group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormComponent } from "./reactive-form/reactive-form.component";
+import { FormGroupComponent } from './form-group/form-group.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ReactiveFormComponent]
+    imports: [RouterOutlet, ReactiveFormComponent,FormGroupComponent]
 })
 export class AppComponent {
   title = 'ANgularProject_2';
---
+++ src/app/form-group/form-group.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'form-group',
+  standalone: true,
+  imports: [ReactiveFormsModule,CommonModule],
+  templateUrl: './form-group.component.html',
+  styleUrl: './form-group.component.css'
+})
+export class FormGroupComponent {
+
+  reactiveForm = new  FormGroup({
+    ename: new FormControl('',Validators.required),
+    salary: new  FormControl('',Validators.required)
+  });
+
+  onSubmit(){
+    console.log("Name : "+this.reactiveForm.get('ename')?.value);
+    console.log("Salary : "+this.reactiveForm.get('salary')?.value);
+  }
+
+  get f( ) { return this.reactiveForm.controls; }
+} 
